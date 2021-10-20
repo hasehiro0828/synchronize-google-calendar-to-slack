d40a1d04bfe411f52d0ad79b94ca219e1c65d2a0
Hiroto Hasegawa <hiroto_hasegawa@voyagegroup.com>
2021-10-20 20:58:39 +0900

    feat: 環境構築

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base', 'plugin:import/recommended', 'plugin:import/typescript', 'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index", "object", "type"],
+        // pathGroups: [
+        //   {
+        //     pattern: "@alias/**",
+        //     group: "parent",
+        //     position: "before",
+        //   },
+        // ],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        // alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ".",
+      },
+    },
+  },
+};
